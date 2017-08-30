929769ac60dabc17d1ddecb2232e91298db3b1f5
linkcoding <kenny.lin.91@gmail.com>
2017-08-29 23:42:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  let counter = 0
+
+  function addBump() {
+    return counter += 1;
+  }
+
+  function getBumps(){
+    return counter;
+  }
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType) {
+  return function assignDevice(deadlyDevice) {
+    return {"animalType": animalType, "deadlyDevice": deadlyDevice}
+  }
+}
+
+let sharkCreator = createAnimal("Shark")
+let sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+let sharkWithFrickinCannon= sharkCreator("Cannon")
